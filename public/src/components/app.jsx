146b4ee4eba8b78c0c2cc243eab2ace304b483cc
Teddy Kim <teddykim09@gmail.com>
2021-02-23 14:08:00 -0800

    Set up basic layout for app component and index html

---
+++ public/src/components/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div id='app-main-container'>
+
+        </div>
+    );
+};
+
+export default App;
--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use(express.static(__dirname + '/../public'));
+app.use(express.static(__dirname + '/../public/dist'));
 
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`);
