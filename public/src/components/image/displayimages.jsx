0d2e42827464625c9a0390946a75a0d578f4ac47
Teddy Kim <teddykim09@gmail.com>
2021-02-25 09:28:52 -0800

    Built out the display images component and added styling

--- public/src/components/image/displayimages.jsx
+++ public/src/components/image/displayimages.jsx
@@ -1,16 +1,23 @@
 import React, { useContext } from 'react';
 import { ImageContext } from './image.jsx';
+import { GridList, GridListTile, GridListTileBar } from '@material-ui/core';
 
 export default function DisplayImages() {
     const { images } = useContext(ImageContext);
-
+ 
     return (
-        <div id='display-images-container'>
-            {images.map(image => {
-                return (
-                    <img src={image.urls.full} />
-                )
-            })}
-        </div>
+    <div className='displayed-images-container'>
+        <GridList cols={5} spacing={20} cellHeight={300} className='grid-list-container'>
+        {images.map((image) => (
+            <GridListTile key={image.id} className='grid-list-tile'>
+            <img src={image.urls.small} alt={image.alt_description} />
+            <GridListTileBar
+                title={image.description || image.alt_description}
+                subtitle={<span>by: {image.user.name}</span>}
+            />
+            </GridListTile>
+        ))};
+        </GridList>
+    </div>
     );
 };
