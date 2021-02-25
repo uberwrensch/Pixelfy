96c1b4cce42f05f3e84fb7fe6cf9e615016f0b2b
Teddy Kim <teddykim09@gmail.com>
2021-02-25 11:45:04 -0800

    Added styling to the search components

--- public/src/components/search/search.jsx
+++ public/src/components/search/search.jsx
@@ -5,6 +5,7 @@ import SearchOptions from './searchoptions.jsx';
 export default function Search() {
     return (
         <div id='search-container'>
+            <img id='pixelfy-icon' src='../../../assets/pixelfy.png'/>
             <SearchBar />
             <SearchOptions />
         </div>
